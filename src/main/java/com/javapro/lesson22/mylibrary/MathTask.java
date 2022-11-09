13609a6c7597d9960c38a30aaa1b7a7c5d0192d2
Andrii <andriutsa1@ukr.net>
2022-11-09 02:22:11 +0200

    create jar

---
+++ src/main/java/com/javapro/lesson22/mylibrary/MathTask.java
@@ -0,0 +1,27 @@
+package com.javapro.lesson22.mylibrary;
+
+/**
+ * @author Andrii Andriutsa on 08.11.2022
+ */
+public class MathTask {
+
+  /**
+   * Display the first value terms of the Fibonacci sequence
+   * Each number in the Fibonacci sequence is the sum of the two numbers that precede it.
+   * */
+    public void fibonacci(int value) {
+      System.out.println("The first "+ value +" terms of the Fibonacci sequence");
+      int n0 =0;
+      int n1=1;
+      int n2;
+      System.out.print(n0 + " " + n1 + " ");
+      for (int i = 3; i <= value; i++) {
+        n2 = n0 + n1;
+        System.out.print(n2 + " ");
+        n0 = n1;
+        n1 = n2;
+      }
+      System.out.println();
+    }
+  }
+
